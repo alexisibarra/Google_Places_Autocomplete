72242fcb29760e02b19ff46659ebf98cff3561ba
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2020-10-29 12:03:29 -0300

    refactor to typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { AutocompleteAddress } from "Components/AutocompleteAddress/AutocompleteAddress";
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <AutocompleteAddress />
+    </div>
+  );
+}
+
+export default App;
